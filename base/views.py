2f62d9cf774db91095dfa1e50a6596f62e100286
Kunal Chhetri <kunalchettri8945@gmail.com>
2025-03-04 22:09:39 +0530

    Initial commit - Django Portfolio

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def index2(request):
+    return render(request,'index-2-light.html')
+# def index(request):
+#     return render(request,'index.html')
+# def index(request):
+#     return render(request,'index.html')
+# def index(request):
+#     return render(request,'index.html')
+# def index(request):
+#     return render(request,'index.html')
+# def index(request):
+# return render(request,'index.html')
